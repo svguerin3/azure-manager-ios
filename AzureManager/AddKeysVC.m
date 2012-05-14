44e75750464a93d5b88e69322d1986a90d13ad54
Vincent Guerin <svguerin3@hotmail.com>
2012-05-14 15:15:46 -0500

    updated some comments

--- AzureManager/AddKeysVC.m
+++ AzureManager/AddKeysVC.m
@@ -3,7 +3,7 @@
 //  AzureManager
 //
 //  Created by Vincent Guerin on 5/10/12.
-//  Copyright (c) 2012 Vurgood Apps. All rights reserved.
+//  Copyright (c) 2012 Neudesic. All rights reserved.
 //
 
 #import "AddKeysVC.h"
--- AzureManager/PropertyDetailVC.m
+++ AzureManager/PropertyDetailVC.m
@@ -3,7 +3,7 @@
 //  AzureManager
 //
 //  Created by Vincent Guerin on 5/8/12.
-//  Copyright (c) 2012 Vurgood Apps. All rights reserved.
+//  Copyright (c) 2012 Neudesic. All rights reserved.
 //
 
 #import "PropertyDetailVC.h"
