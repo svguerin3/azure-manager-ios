032dac5a15f7a42bd1ab32d6f3cbf74f8b567ca5
Vincent Guerin <svguerin3@hotmail.com>
2012-06-27 10:07:12 -0500

    some cleanup

--- AzureManager/HostedServicesListVC.m
+++ AzureManager/HostedServicesListVC.m
@@ -3,7 +3,7 @@
 //  AzureManager
 //
 //  Created by Vincent Guerin on 6/12/12.
-//  Copyright (c) 2012 Vurgood Apps. All rights reserved.
+//  Copyright (c) 2012 Neudesic. All rights reserved.
 //
 
 #import "HostedServicesListVC.h"
